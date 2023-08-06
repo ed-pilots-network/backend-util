b4f7a955a12875bbda1554f04e8353add40614a6
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2023-08-06 21:23:36 +0200

    #3 - adding enums and bumping version (#4)
    * #3 - adding enums and bumping version
    
    * #3 - formatted Response topic name
    
    * #3 - formatted Response topic name
    
    ---------
    
    Co-authored-by: Pieter Van Eeckhout <pieter.vaneeckhout@hoakt.com>

---
+++ src/main/java/io/edpn/backend/util/Topic.java
@@ -0,0 +1,42 @@
+package io.edpn.backend.util;
+
+public interface Topic {
+
+    enum Request {
+        STATION_ARRIVAL_DISTANCE_REQUEST("stationArrivalDistanceRequest"),
+        STATION_MAX_LANDING_PAD_SIZE_REQUEST("stationMaxLandingPadSizeRequest"),
+        STATION_IS_PLANETARY_REQUEST("stationIsPlanetaryRequest"),
+        STATION_REQUIRE_ODYSSEY_REQUEST("stationRequireOdysseyRequest"),
+        SYSTEM_COORDINATES_REQUEST("systemCoordinatesRequest"),
+        SYSTEM_ELITE_ID_REQUEST("systemEliteIdRequest");
+
+        private final String topicName;
+
+        Request(String topicName) {
+            this.topicName = topicName;
+        }
+
+        public String getTopicName() {
+            return topicName;
+        }
+    }
+
+    enum Response {
+        STATION_ARRIVAL_DISTANCE_RESPONSE("%s_stationArrivalDistanceResponse"),
+        STATION_MAX_LANDING_PAD_SIZE_RESPONSE("%s_stationMaxLandingPadSizeResponse"),
+        STATION_IS_PLANETARY_RESPONSE("%s_stationIsPlanetaryResponse"),
+        STATION_REQUIRE_ODYSSEY_RESPONSE("%s_stationRequireOdysseyResponse"),
+        SYSTEM_COORDINATES_RESPONSE("%s_systemCoordinatesResponse"),
+        SYSTEM_ELITE_ID_RESPONSE("%s_systemEliteIdResponse");
+
+        private final String topicName;
+
+        Response(String topicName) {
+            this.topicName = topicName;
+        }
+
+        public String getFormattedTopicName(Module module) {
+            return String.format(topicName, module.getName());
+        }
+    }
+}
