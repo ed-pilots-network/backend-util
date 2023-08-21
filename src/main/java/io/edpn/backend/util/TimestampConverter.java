ed35b20fdc8890c8781bac4703a812a5c4745b74
Pieter Van Eeckhout <pieter.vaneeckhout@hoakt.com>
2023-08-21 21:35:28 +0200

    extending number of supported millisecond accuracy

--- src/main/java/io/edpn/backend/util/TimestampConverter.java
+++ src/main/java/io/edpn/backend/util/TimestampConverter.java
@@ -5,24 +5,25 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
 
 public class TimestampConverter {
 
-    private static final DateTimeFormatter FORMATTER_WITH_MS = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSSX");
-    private static final DateTimeFormatter FORMATTER_WITHOUT_MS = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssX");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME;
+
+    private TimestampConverter() {
+    }
 
     public static LocalDateTime convertToLocalDateTime(String timestamp) {
         try {
-            ZonedDateTime zonedDateTime = ZonedDateTime.parse(timestamp, FORMATTER_WITH_MS);
-            return zonedDateTime.withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
-        } catch (DateTimeParseException e) {
-            try {
-                ZonedDateTime zonedDateTime = ZonedDateTime.parse(timestamp, FORMATTER_WITHOUT_MS);
-                return zonedDateTime.withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
-            } catch (DateTimeParseException ex) {
-                throw new IllegalArgumentException("Invalid timestamp format: " + timestamp, ex);
-            }
+            TemporalAccessor zonedDateTime = formatter.parseBest(timestamp, ZonedDateTime::from,
+                    LocalDateTime::from);
+            return LocalDateTime.from(zonedDateTime)
+                    .atOffset(ZoneOffset.UTC)
+                    .toLocalDateTime();
+        } catch (DateTimeParseException ex) {
+            throw new IllegalArgumentException("Invalid timestamp format: " + timestamp, ex);
         }
     }
-
 }
+
