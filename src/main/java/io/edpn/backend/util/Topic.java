7ada7c12a7689431797f0b9c4d0a2ddc478ae0bf
pveeckhout <vaneeckhout.pieter@gmail.com>
2023-11-21 20:45:33 +0100

    #133 - making response topic name a usable constant per module

--- src/main/java/io/edpn/backend/util/Topic.java
+++ src/main/java/io/edpn/backend/util/Topic.java
@@ -38,5 +38,14 @@ public interface Topic {
         public String getFormattedTopicName(Module module) {
             return String.format(topicName, module.getName());
         }
+
+        public static class Constants {
+            public static final String TRADE_STATION_ARRIVAL_DISTANCE = Response.STATION_ARRIVAL_DISTANCE.getFormattedTopicName(Module.TRADE);
+            public static final String TRADE_STATION_MAX_LANDING_PAD_SIZE = Response.STATION_MAX_LANDING_PAD_SIZE.getFormattedTopicName(Module.TRADE);
+            public static final String TRADE_STATION_IS_PLANETARY = Response.STATION_IS_PLANETARY.getFormattedTopicName(Module.TRADE);
+            public static final String TRADE_STATION_REQUIRE_ODYSSEY = Response.STATION_REQUIRE_ODYSSEY.getFormattedTopicName(Module.TRADE);
+            public static final String TRADE_SYSTEM_COORDINATES = Response.SYSTEM_COORDINATES.getFormattedTopicName(Module.EXPLORATION);
+            public static final String TRADE_SYSTEM_ELITE_ID = Response.SYSTEM_ELITE_ID.getFormattedTopicName(Module.EXPLORATION);
+        }
     }
 }
